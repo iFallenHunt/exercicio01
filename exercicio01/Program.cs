298a5f900b1971edf9aea22076896c677e3f000b
Lucas Santos <lukas_santos.silva@hotmail.com>
2022-01-19 09:22:46 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace exerc01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite um numero?");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero < -1)
+            {
+                Console.WriteLine("Negativo!");
+            }
+            else
+            {
+                Console.WriteLine("Não Negativo");
+            }
+        }
+    }
+}
